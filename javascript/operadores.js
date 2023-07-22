7e1faccca0ebef0405d25c165a06004f5cc0652b
joseweslley03 <weslleyphb03@gmail.com>
2023-07-21 21:13:59 -0300

    aprendendo js

---
+++ javascript/operadores.js
@@ -0,0 +1,29 @@
+//Aritmeticos
+// + adição
+// - subtração
+// * multiplicação
+// / multiplicação
+let numeroUm = 5
+let numeroDois = 6
+let numeroTres = -1
+console.log(numeroDois + numeroTres)
+
+//Relacionais
+// > maior que
+// < menor que
+// != diferente
+// == igual
+//    ===
+// >= maior ou igual
+// <= menor ou igual
+
+let numeroString = '6'
+console.log(numeroDois === numeroString)
+// = Atribuição
+
+//lógicos
+// && E
+// || OU
+// ! NÃO
+
+console.log(!(numeroDois === numeroString))
---
+++ javascript/variaveis.js
@@ -0,0 +1,16 @@
+
+//usando var ou nada
+    //identificador
+    //  atribuicao
+      // valor
+      // possivel erro com undefined
+console.log(nome);
+var nome="Weslley"
+sobrenome="Sousa"
+
+//não permite a execução antes da criação (ERRO)
+let nome2 = "Senac"
+console.log(typeof nome2)
+
+const pi=3.14
+console.log(typeof pi)
